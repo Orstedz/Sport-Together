0c89b93554cbc5a4c495077460030b3373ca31d7
Tron <trongtrawork@gmail.com>
2024-12-10 23:55:46 +0700

    build: base for main.tsx

---
+++ src/app/main.tsx
@@ -0,0 +1,9 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+{ "@todo Imported app's root component -- assigned to Nguyen Phat Tai" }
+
+createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+        {"@todo Replace this with app's root component -- assigned to Nguyen Phat Tai"}
+    </StrictMode>,
+)
