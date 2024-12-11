06ddc3e621a04025e2257ce04c31fdd81ed4b294
Minh Pham <giaminh2042004@gmail.com>
2024-12-11 19:40:16 +0700

    build: add import path

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./header/header";
 import SignForm from "./signForm/signForm";
+import "./register.css";
 
 const RegisterPage: React.FC = () => {
   return (
