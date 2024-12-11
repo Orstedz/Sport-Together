f41a2e8ccee9a02a4952c71c2f6a9a9ec13f141b
Minh Pham <giaminh2042004@gmail.com>
2024-12-12 00:29:13 +0700

    build: base for page.tsx

---
+++ src/app/recoverPassword/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "../../components/header/preHeader";
+import RecoverPasswordForm from "./recoverPasswordForm/recoverPasswordForm";
+import "./logIn.css";
+
+const LogInPage: React.FC = () => {
+  return (
+    <div className="bg-gray-100 min-h-screen flex flex-col">
+      <Header />
+      <div className="flex-grow flex items-center justify-center">
+        <RecoverPasswordForm />
+      </div>
+    </div>
+  );
+};
+
+export default LogInPage;
