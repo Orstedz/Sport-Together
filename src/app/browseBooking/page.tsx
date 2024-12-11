f006ba12da410db1e068063d2ab0166a148ca0cd
Tron <trongtrawork@gmail.com>
2024-12-11 13:51:35 +0700

    style: add more mock yards for development purposes

--- src/app/browseBooking/page.tsx
+++ src/app/browseBooking/page.tsx
@@ -41,6 +41,38 @@ const browseBookingPage: React.FC = () => {
             rating: 4,
             size: 'fustalcourt',
             price: '230,000đ - 350,000đ/giờ'
+        },
+        {
+            id: '4',
+            name: 'Sân Thống Nhất',
+            address: '1 Lê Duẩn, phường Bến Nghé, quận 1, TP.HCM',
+            rating: 5,
+            size: '11playerscourt',
+            price: '500,000đ - 700,000đ/giờ'
+        },
+        {
+            id: '5',
+            name: 'Sân Hoa Lư',
+            address: '2 Đinh Tiên Hoàng, phường Đa Kao, quận 1, TP.HCM',
+            rating: 4,
+            size: '7playerscourt',
+            price: '200,000đ - 350,000đ/giờ'
+        },
+        {
+            id: '6',
+            name: 'Sân Phú Thọ',
+            address: '221 Lý Thường Kiệt, phường 15, quận 11, TP.HCM',
+            rating: 3,
+            size: '5playerscourt',
+            price: '150,000đ - 250,000đ/giờ'
+        },
+        {
+            id: '7',
+            name: 'Sân Kỳ Hòa',
+            address: '796 Sư Vạn Hạnh, phường 12, quận 10, TP.HCM',
+            rating: 4,
+            size: 'fustalcourt',
+            price: '230,000đ - 350,000đ/giờ'
         }
     ];
 
