e5f232a4c632301278278d9cbe10aaf717443b43
Minh Pham <giaminh2042004@gmail.com>
2024-12-12 00:33:33 +0700

    build: routing for recover password page in main.tsx

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import BrowseBooking from "./browseBooking/page";
 import Register from "./register/page";
 import LogIn from "./login/page";
+import RecoverPasswordPage from "./recoverPassword/page";
 {
   /** @todo Imported app's root -- assigned to Nguyen Phat Tai */
 }
@@ -21,6 +22,7 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
         {/** @description Route to register*/}
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<LogIn />} />
+        <Route path="/recoverPassword" element={<RecoverPasswordPage />} />
         {/** @todo Add route to Welcome Page -- assigned to Nguyen Phat Tai*/}
         <Route path="/" element={<div>Welcome to the Booking App</div>} />
       </Routes>
