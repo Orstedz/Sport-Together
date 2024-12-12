aedfbc69c1b3d34cc8a8a02317fc9f82484736c6
Tron <trongtrawork@gmail.com>
2024-12-12 20:06:11 +0700

    build: base for proceed payment page

---
+++ src/app/proceedPayment/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../../components/header/header';
+
+const ProceedPayment: React.FC = () => {
+    return (
+        <div className="flex flex-col min-h-screen bg-gray-100 overflow-hidden">
+            <Header />
+        </div>
+    );
+}
+
+export default ProceedPayment;
