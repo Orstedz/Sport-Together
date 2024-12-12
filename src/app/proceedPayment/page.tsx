60a94dd55cf51ebcc65fa376f00511701192655e
Tron <trongtrawork@gmail.com>
2024-12-12 20:13:01 +0700

    build: function to generate a random QR code

--- src/app/proceedPayment/page.tsx
+++ src/app/proceedPayment/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../../components/header/header';
+import RandQRCode from './genQR/genQR';
 
 const ProceedPayment: React.FC = () => {
     return (
         <div className="flex flex-col min-h-screen bg-gray-100 overflow-hidden">
             <Header />
+            <RandQRCode />
         </div>
     );
 }
