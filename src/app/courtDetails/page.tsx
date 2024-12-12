23b73c0b1ef5f77b305135d3d907ad1e26074b33
Minh Pham <giaminh2042004@gmail.com>
2024-12-12 20:49:52 +0700

    build: base for page.tsx

---
+++ src/app/courtDetails/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import CourtInfo from "./courtInfo/courtInfo";
+import DetailTable from "./detailTable/datailTable";
+
+const CourtDetailsPage = () => {
+  return (
+    <div>
+      <CourtInfo />
+      <DetailTable />
+    </div>
+  );
+};
+
+export default CourtDetailsPage;
