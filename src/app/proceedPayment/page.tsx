1813f5ca18c372013620e51a8ddfdf6957cbafb2
Tron <trongtrawork@gmail.com>
2024-12-12 20:26:28 +0700

    build: proceed payment page structure

--- src/app/proceedPayment/page.tsx
+++ src/app/proceedPayment/page.tsx
@@ -4,11 +4,37 @@ import RandQRCode from './genQR/genQR';
 
 const ProceedPayment: React.FC = () => {
     return (
-        <div className="flex flex-col min-h-screen bg-gray-100 overflow-hidden">
+        <div className="flex flex-col min-h-screen bg-gray-50 overflow-hidden">
             <Header />
-            <RandQRCode />
+            <div className="text-center mt-16 px-4">
+                <h1 className="text-4xl font-bold text-green-600 mb-6">
+                    SCAN QR CODE AND FINISH PAYMENT
+                </h1>
+                <button
+                    className="px-6 py-3 border-2 border-green-700 text-green-700 rounded-lg hover:bg-green-700 hover:text-white text-lg font-bold"
+                >
+                    {"<<"}
+                </button>
+            </div>
+
+            <div className="flex justify-center my-12">
+                <div className="border-2 border-gray-300 p-4 rounded-lg bg-white shadow-md">
+                    <RandQRCode />
+                </div>
+            </div>
+
+            <div className="text-center mt-4 px-4">
+                <p className="text-gray-700 text-lg mb-4">
+                    Please finish payment within 10 minutes. Once done, kindly press the button below.
+                </p>
+                <button
+                    className="px-8 py-4 bg-green-700 text-white rounded-lg hover:bg-green-600 text-lg font-bold"
+                >
+                    Already Transferred
+                </button>
+            </div>
         </div>
     );
-}
+};
 
 export default ProceedPayment;
