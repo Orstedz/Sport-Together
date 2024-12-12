8d7661416386be57281a0b35762a323cb459fbd1
Minh Pham <giaminh2042004@gmail.com>
2024-12-12 16:16:47 +0700

    build: add routing in main.tsx

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import BrowseBooking from "./browseBooking/page";
-import WelcomePage from "./welcome/page";
+import Register from "./register/page";
+import LogIn from "./login/page";
+import RecoverPasswordPage from "./recoverPassword/page";
 {
   /** @todo Imported app's root -- assigned to Nguyen Phat Tai */
 }
@@ -17,9 +19,12 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
         {/** @description Route to browseBooking*/}
         <Route path="/browseBooking" element={<BrowseBooking />} />
         {/** @todo Add route to Login-Register pages -- assigned to Pham Gia Minh*/}
-
+        {/** @description Route to register*/}
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/recoverPassword" element={<RecoverPasswordPage />} />
         {/** @todo Add route to Welcome Page -- assigned to Nguyen Phat Tai*/}
-        <Route path="/" element={<WelcomePage />} />
+        <Route path="/" element={<div>Welcome to the Booking App</div>} />
       </Routes>
     </Router>
   </React.StrictMode>
