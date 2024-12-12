9c2d9080fd294e536bef7cb5ce1091ec65b4947f
Tron <trongtrawork@gmail.com>
2024-12-12 14:49:53 +0700

    build: default route for payment page

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import BrowseBooking from './browseBooking/page';
+import Payment from './payment/page';
 import WelcomePage from './welcome/page';
 { /** @todo Imported app's root -- assigned to Nguyen Phat Tai */ }
 {/** @todo Imported login and register page -- assigned to Pham Gia Minh */ }
@@ -10,14 +11,14 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
         <Router>
             <Routes>
-                {/** @description Route to browseBooking*/}
                 <Route path="/browseBooking" element={<BrowseBooking />} />
+                <Route path="/payment" element={<Payment />} />
                 {/** @todo Add route to Login-Register pages -- assigned to Pham Gia Minh*/}
 
                 {/** @todo Add route to Welcome Page -- assigned to Nguyen Phat Tai*/}
-                <Route path="/" element={<WelcomePage/>} />
+                <Route path="/" element={<WelcomePage />} />
             </Routes>
-            
+
         </Router>
     </React.StrictMode>
 );
---
+++ src/app/payment/page.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import Header from '../../components/header/header';
+
+const PaymentPage: React.FC = () => {
+    return (
+        <div className="min-h-screen bg-gray-100 flex flex-col">
+            <Header />
+        </div>
+    );
+}
+
+export default PaymentPage;
