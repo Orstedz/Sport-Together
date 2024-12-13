b50e5da446842553807c5451f9f34ce35cc747be
Minh Pham <giaminh2042004@gmail.com>
2024-12-13 21:23:32 +0700

    style: change like/dislike icon

--- src/app/courtDetails/detailTable/rating.tsx
+++ src/app/courtDetails/detailTable/rating.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { AiOutlineDislike, AiOutlineLike } from "react-icons/ai";
 
 const Rating = () => {
   const ratings = [5, 4, 3, 2, 1]; // Star ratings
@@ -121,40 +122,12 @@ const Rating = () => {
               {/* Like/Dislike Actions */}
               <div className="flex items-center ml-auto mt-2 text-gray-600">
                 <button className="flex items-center mr-4">
-                  {/* Like Icon */}
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="w-5 h-5 mr-1"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M6.633 10.25c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 0 1 2.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 0 0 .322-1.672V2.75a.75.75 0 0 1 .75-.75 2.25 2.25 0 0 1 2.25 2.25c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282m0 0h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 0 1-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 0 0-1.423-.23H5.904"
-                    />
-                  </svg>
-                  <span>Like (5)</span>
+                  <AiOutlineLike className="w-7 h-7" />
+                  <span className="ml-2">Like (5)</span>
                 </button>
                 <button className="flex items-center">
-                  {/* Dislike Icon */}
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="w-5 h-5 mr-1"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M7.498 15.25H4.372c-1.026 0-1.945-.694-2.054-1.715a12.137 12.137 0 0 1-.068-1.285c0-2.848.992-5.464 2.649-7.521C5.287 4.247 5.886 4 6.504 4h4.016a4.5 4.5 0 0 1 1.423.23l3.114 1.04a4.5 4.5 0 0 0 1.423.23h1.294"
-                    />
-                  </svg>
-                  <span>Dislike (1)</span>
+                  <AiOutlineDislike className="w-7 h-7" />
+                  <span className="ml-2">Dislike (1)</span>
                 </button>
               </div>
             </div>
