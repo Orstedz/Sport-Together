d6ab71e50e69774502bad5acb12a773f6c313cf8
Tron <trongtrawork@gmail.com>
2024-12-13 16:51:32 +0700

    build: add route for history into header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -43,7 +43,7 @@ const Header: React.FC = () => {
                     {/* History */}
                     <li className="h-full">
                         <a
-                            href="#"
+                            href="/history"
                             className="h-full w-52 text-white bg-green-500 transition-all hover:bg-white hover:text-green-500 flex items-center justify-center uppercase"
                             style={{
                                 fontSize: "24px",
