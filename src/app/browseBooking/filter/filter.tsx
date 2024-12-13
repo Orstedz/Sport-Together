1a553ea5a2c7d13b8a645e1f623779b55d57b677
Tron <trongtrawork@gmail.com>
2024-12-13 15:25:00 +0700

    style: adjust filter component

--- src/app/browseBooking/filter/filter.tsx
+++ src/app/browseBooking/filter/filter.tsx
@@ -28,21 +28,21 @@ const Filter: React.FC<FilterProps> = ({ onRatingFilter, onSizeFilter }) => {
     };
 
     return (
-        <div className="bg-gray-100 p-6 rounded-lg">
+        <div className="bg-gray-100 p-4 rounded-lg">
             {/* Size Filter */}
             <div className="mb-4">
                 <div className="flex items-center justify-between">
-                    <h2 className="block font-medium mb-2 text-green-500" style={{ fontSize: '34px' }}>Size</h2>
+                    <h2 className="block font-medium mb-2 text-green-500" style={{ fontSize: '30px' }}>Size</h2>
                     <button
                         onClick={() => setIsSizeFilterVisible(!isSizeFilterVisible)}
-                        className="flex items-center justify-center p-4 text-green-500 rounded"
-                        style={{ backgroundColor: 'transparent', fontSize: '35px' }}
+                        className="flex items-center justify-center p-2 text-green-500 rounded"
+                        style={{ backgroundColor: 'transparent', fontSize: '32px' }}
                     >
                         {isSizeFilterVisible ? '▲' : '▼'}
                     </button>
                 </div>
                 {isSizeFilterVisible && (
-                    <div className="flex flex-col space-y-2" style={{ fontSize: '32px' }}>
+                    <div className="flex flex-col space-y-2" style={{ fontSize: '22px' }}>
                         {[
                             { label: '7 players', value: '7playerscourt' },
                             { label: '5 players', value: '5playerscourt' },
@@ -70,11 +70,11 @@ const Filter: React.FC<FilterProps> = ({ onRatingFilter, onSizeFilter }) => {
             {/* Rating Filter */}
             <div className="mb-4">
                 <div className="flex items-center justify-between">
-                    <h2 className="block font-medium mb-2 text-green-500" style={{ fontSize: '34px' }}>Rating</h2>
+                    <h2 className="block font-medium mb-2 text-green-500" style={{ fontSize: '30px' }}>Rating</h2>
                     <button
                         onClick={() => setIsRatingFilterVisible(!isRatingFilterVisible)}
                         className="flex items-center justify-center p-4 text-green-500 rounded"
-                        style={{ backgroundColor: 'transparent', fontSize: '35px' }}
+                        style={{ backgroundColor: 'transparent', fontSize: '32px' }}
                     >
                         {isRatingFilterVisible ? '▲' : '▼'}
                     </button>
@@ -94,7 +94,7 @@ const Filter: React.FC<FilterProps> = ({ onRatingFilter, onSizeFilter }) => {
                                     className="mr-2 cursor-pointer"
                                     style={{ width: '20px', height: '20px' }}
                                 />
-                                <span className="text-4xl">
+                                <span className="text-2xl">
                                     <span className="text-yellow-500">{'★'.repeat(starCount)}</span>
                                     <span className="text-gray-300">{'★'.repeat(5 - starCount)}</span>
                                 </span>
