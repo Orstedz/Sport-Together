09448390a09d528ecb0a0a2cad0437c8be36d656
Minh Pham <giaminh2042004@gmail.com>
2024-12-13 19:35:16 +0700

    build: add header for the page

--- src/app/courtDetails/page.tsx
+++ src/app/courtDetails/page.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import CourtInfo from "./courtInfo/courtInfo";
 import DetailTable from "./detailTable/datailTable";
+import PreHeader from "../../components/header/preHeader";
 
 const CourtDetailsPage = () => {
   const [activeTab, setActiveTab] = useState<"description" | "rating">(
@@ -8,40 +9,43 @@ const CourtDetailsPage = () => {
   );
 
   return (
-    <div className="max-w-4xl mx-auto bg-white shadow-lg rounded-lg border border-gray-200 overflow-hidden">
-      {/* Banner Section */}
-      <CourtInfo />
+    <>
+      <PreHeader />
+      <div className="max-w-4xl mx-auto overflow-hidden">
+        {/* Banner Section */}
+        <CourtInfo />
 
-      {/* Box Section */}
-      <div className="p-4">
-        {/* Header */}
-        <div className="flex border-b mb-4">
-          <button
-            onClick={() => setActiveTab("description")}
-            className={`flex-1 py-2 text-center ${
-              activeTab === "description"
-                ? "text-green-600 border-b-2 border-green-600"
-                : "text-gray-600"
-            } font-bold`}
-          >
-            Mô tả
-          </button>
-          <button
-            onClick={() => setActiveTab("rating")}
-            className={`flex-1 py-2 text-center ${
-              activeTab === "rating"
-                ? "text-green-600 border-b-2 border-green-600"
-                : "text-gray-600"
-            } font-bold`}
-          >
-            Đánh giá
-          </button>
+        {/* Box Section */}
+        <div className="p-4 bg-gray-100 rounded-md">
+          {/* Header */}
+          <div className="flex border-b mb-4">
+            <button
+              onClick={() => setActiveTab("description")}
+              className={`flex-1 py-2 text-center ${
+                activeTab === "description"
+                  ? "text-green-600 border-b-2 border-green-600"
+                  : "text-gray-600"
+              } font-bold`}
+            >
+              Mô tả
+            </button>
+            <button
+              onClick={() => setActiveTab("rating")}
+              className={`flex-1 py-2 text-center ${
+                activeTab === "rating"
+                  ? "text-green-600 border-b-2 border-green-600"
+                  : "text-gray-600"
+              } font-bold`}
+            >
+              Đánh giá
+            </button>
+          </div>
+
+          {/* Content */}
+          <DetailTable activeTab={activeTab} />
         </div>
-
-        {/* Content */}
-        <DetailTable activeTab={activeTab} />
       </div>
-    </div>
+    </>
   );
 };
 
