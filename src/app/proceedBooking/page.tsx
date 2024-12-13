043cc9f1de4de396e6241789e941970cc22d280e
Tron <trongtrawork@gmail.com>
2024-12-13 20:18:04 +0700

    build: passing the court to proceedBooking

--- src/app/proceedBooking/page.tsx
+++ src/app/proceedBooking/page.tsx
@@ -3,11 +3,22 @@
  */
 
 import React from 'react';
+import { useLocation } from 'react-router-dom';
+import Court from '../../interfaces/court';
 
-const ProceedBooking: React.FC = () => {
+const ProceedBooking: React.FC<Court> = () => {
+    const location = useLocation();
+    const court = location.state?.court as Court;
+
     return (
         <div>
             <h1>Proceed Payment</h1>
+            {court && (
+                <div>
+                    <h2>{court.name}</h2>
+                    <p>{court.address}</p>
+                </div>
+            )}
         </div>
     );
 }
