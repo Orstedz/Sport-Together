f706ca1fa8fdc8a016485b4c14323bc1600b42c8
Tron <trongtrawork@gmail.com>
2024-12-14 13:58:37 +0700

    fix: routing for details

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -23,13 +23,13 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
         <Route path="/history" element={<History></History>} />
         <Route path="/booking" element={<Booking />} />
         <Route path="/booking/proceed" element={<ProceedBooking />} />
-        <Route path="/booking/detail/proceed" element={<ProceedBooking />} />
+        <Route path="/booking/details/proceed" element={<ProceedBooking />} />
         <Route path="/payment" element={<Payment />} />
         <Route path="/payment/proceed" element={<ProceedPayment />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<LogIn />} />
         <Route path="/recoverPassword" element={<RecoverPasswordPage />} />
-        <Route path="/booking/detail" element={<CourtDetailsPage />} />
+        <Route path="/booking/details" element={<CourtDetailsPage />} />
         <Route path="/" element={<WelcomePage></WelcomePage>} />
       </Routes>
     </Router>
