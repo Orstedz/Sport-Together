4d1fd3d3a454a79ac0556794992064b7c2d0ae9e
Tron <trongtrawork@gmail.com>
2024-12-14 22:30:27 +0700

    build: create a back button for detail

--- src/app/courtDetails/page.tsx
+++ src/app/courtDetails/page.tsx
@@ -5,7 +5,7 @@ import DetailTable from "./detailTable/datailTable";
 import Header from "../../components/header/header";
 
 const CourtDetailsPage: React.FC = () => {
-
+  const navigate = useNavigate();
   const location = useLocation();
   const { details } = location.state;
 
@@ -17,6 +17,14 @@ const CourtDetailsPage: React.FC = () => {
     <>
       <Header />
       <div className="max-w-7xl mx-auto overflow-hidden">
+        <button
+          className="absolute top-8 left-28 px-10 py-3 border-2 border-green-700 text-green-700 rounded-lg hover:bg-green-700 hover:text-white text-lg font-bold"
+          onClick={() => navigate('./../', { state: { details } })}
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M21 16.811c0 .864-.933 1.406-1.683.977l-7.108-4.061a1.125 1.125 0 0 1 0-1.954l7.108-4.061A1.125 1.125 0 0 1 21 8.689v8.122ZM11.25 16.811c0 .864-.933 1.406-1.683.977l-7.108-4.061a1.125 1.125 0 0 1 0-1.954l7.108-4.061a1.125 1.125 0 0 1 1.683.977v8.122Z" />
+          </svg>
+        </button>
         <CourtInfo details={details} />
 
         <div className="bg-gray-100 rounded-md">
