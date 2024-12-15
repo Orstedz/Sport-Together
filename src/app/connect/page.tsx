52069bd7a7e37809377b1b38a45dc518d49c00c4
Minh Pham <giaminh2042004@gmail.com>
2024-12-15 23:26:00 +0700

    build: add header for the page

--- src/app/connect/page.tsx
+++ src/app/connect/page.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import EventList from "./eventList/eventList";
+import Header from "../../components/header/header";
 
 const Page: React.FC = () => {
   return (
     <>
+      <Header />
       <div className=" min-h-screen min-w-screen mx-auto container">
         <EventList />
       </div>
