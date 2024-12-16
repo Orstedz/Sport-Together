f499c49de1e8f616cef79a73ef11f37bbde22cbd
Minh Pham <giaminh2042004@gmail.com>
2024-12-16 09:35:11 +0700

    build: change interface and data

--- src/app/connect/eventData.ts
+++ src/app/connect/eventData.ts
@@ -1,51 +1,66 @@
-import { events } from './eventInterface';
+import  events  from './eventInterface';
 
 const eventData: events[] = [
     new events(
       {
-        location: "Sân Thành Thắng, quận 12",
+        id: 1,
+        name: "Sân Thành Thắng",
         date: "Thứ 7, 20/12/2024, 17:30",
         host: "Trà Minh Trọng",
         playersNeeded: 5,
         level: "Trung bình khá",
         price: "25k",
         description: "",
-        name: "PickleBall Championship",
         sport: "PickleBall",
-        province: "Hà Nội",
-        district: "Quận 1",
+        province: "Hồ Chí Minh",
+        district: "Quận 12",
       },
     ),
     new events(
       {
-        location: "Sân Bình Minh, quận 10",
+        id: 2,
+        name: "Sân Bình Minh",
         date: "Chủ nhật, 21/12/2024, 18:00",
         host: "Nguyễn Văn An",
         playersNeeded: 8,
         level: "Cao",
         price: "50k",
         description: "Cần thêm người để đủ sân.",
-        name: "Badminton Open",
         sport: "Badminton",
-        province: "Đà Nẵng",
-        district: "Quận 3",
+        province: "Hồ Chí Minh",
+        district: "Quận 1",
       },
     ),
     new events(
       {
-        location: "Sân Ngọc Thảo, quận 5",
+        id: 3,
+        name: "Sân Ngọc Thảo",
         date: "Thứ 2, 22/12/2024, 19:00",
         host: "Lê Hải Đăng",
         playersNeeded: 4,
         level: "Trung bình",
         price: "20k",
         description: "Tham gia giao lưu vui vẻ.",
-        name: "Football League",
         sport: "Football",
-        province: "TP. Hồ Chí Minh",
-        district: "Quận 7",
+        province: "Thủ Đức",
+        district: "Quận 9",
       },
-    )
+    ),
+    new events(
+      {
+        id: 4,
+        name: "Sân Ngọc",
+        date: "Thứ 2, 22/12/2024, 19:00",
+        host: "Lê Hải Đăng",
+        playersNeeded: 4,
+        level: "Trung bình",
+        price: "20k",
+        description: "Tham gia giao lưu vui vẻ.",
+        sport: "Football",
+        province: "Thủ Đức",
+        district: "Quận 9",
+      },
+    ),
 ];
 
 export default eventData;
--- src/app/connect/eventInterface.ts
+++ src/app/connect/eventInterface.ts
@@ -1,29 +1,29 @@
 class events {
-  location: string;
+  id: number;
+  name: string;
   date: string;
   host: string;
   playersNeeded: number;
   level: string;
   price: string;
   description: string;
-  name: string;
   sport: string;
   province: string;
   district: string;
 
   constructor(data: events) {
-    this.location = data.location;
+    this.id = data.id;
+    this.name = data.name;
     this.date = data.date;
     this.host = data.host;
     this.playersNeeded = data.playersNeeded;
     this.level = data.level;
     this.price = data.price;
     this.description = data.description;
-    this.name = data.name;
     this.sport = data.sport;
     this.province = data.province;
     this.district = data.district;
   }
 }
 
-export { events };
+export default events ;
