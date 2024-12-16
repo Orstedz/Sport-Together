56c124416a1f0d9b41b33379e3c2759ede670f65
Minh Pham <giaminh2042004@gmail.com>
2024-12-16 09:37:04 +0700

    build: add the filter in the page

--- src/app/connect/page.tsx
+++ src/app/connect/page.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import EventList from "./eventList/eventList";
 import Header from "../../components/header/header";
+import Filter from "./filter/filter";
 
 const Page: React.FC = () => {
   return (
     <>
       <Header />
+      <Filter />
       <div className=" min-h-screen min-w-screen mx-auto container">
         <EventList />
       </div>
