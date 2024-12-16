5dd9848e0b612a342cae2e1589141e259bdf799a
Minh Pham <giaminh2042004@gmail.com>
2024-12-16 15:22:56 +0700

    build: base code for the page base on log in page

---
+++ src/app/createEvent/page.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Header from "../../components/header/preHeader";
+import CreateEventForm from "./createEventForm/createEventForm";
+import "./createEvent.css";
+
+const LogInPage: React.FC = () => {
+  return (
+    <div
+      className="bg-gray-100 min-h-screen flex flex-col bg-cover bg-center"
+      style={{
+        backgroundImage: "url('/assets/Backgrounds/logInPage.jpg')",
+      }}
+    >
+      <Header />
+      <div className="flex-grow flex items-center justify-center">
+        <CreateEventForm />
+      </div>
+    </div>
+  );
+};
+
+export default LogInPage;
