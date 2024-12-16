4e0ce50d4ce8b4539359bcf05a62c6430188e57e
Minh Pham <giaminh2042004@gmail.com>
2024-12-16 11:00:26 +0700

    build: add button to navigate to create event page

--- src/app/connect/page.tsx
+++ src/app/connect/page.tsx
@@ -1,22 +1,39 @@
 import React, { useState } from "react";
-import EventList from "./eventList/eventList";
+import { useNavigate } from "react-router-dom";
 import Header from "../../components/header/header";
 import Filter from "./filter/filter";
+import EventList from "./eventList/eventList";
 import events from "./eventInterface";
 import eventData from "./eventData";
 
 const Page: React.FC = () => {
   const [filteredEvents, setFilteredEvents] = useState<events[]>(eventData);
+  const navigate = useNavigate();
 
   const handleFilter = (filteredData: events[]) => {
     setFilteredEvents(filteredData);
   };
 
+  const handleCreateEventClick = () => {
+    navigate("/connect/create");
+  };
+
   return (
     <>
       <Header />
-      <Filter onFilter={handleFilter} />
-      <div className="min-h-screen min-w-screen mx-auto container">
+      <div className="container mx-auto p-4">
+        <div className="flex justify-end mb-4">
+          <button
+            onClick={handleCreateEventClick}
+            className="bg-green-700 hover:bg-green-800 text-white font-bold text-xl py-2 px-10 rounded-lg"
+            style={{ marginRight: "167px" }}
+          >
+            CREATE EVENT
+          </button>
+        </div>
+        <div className="flex justify-center items-center mb-4">
+          <Filter onFilter={handleFilter} />
+        </div>
         <EventList events={filteredEvents} />
       </div>
     </>
